2e47bc7109f2d3d84489ff0a312d18eef8f16bc7
anubus298 <araristaf@gmail.com>
2024-12-05 01:02:31 +0100

    Modifying dockerfile

--- projet/settings.py
+++ projet/settings.py
@@ -20,7 +20,6 @@ api_key = os.getenv("API_KEY")
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-STATICFILES_DIRS = [BASE_DIR/'staticfiles',]
 STATIC_ROOT = BASE_DIR/'staticfiles'
 
 
@@ -150,6 +149,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
